d98a068cd3200fc600194995f98b4d377159fc4a
kaushal gawri <s3777121@student.rmit.edu.au>
2020-10-11 00:55:43 +0530

    Fixing bugs in first monitoring script

--- first-script.sh
+++ first-script.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 # don't know if it will work
 trap end_monitor USR1
-file = "/usr/src/myFileNew"
-/bin/cat <<EOM >$file
-function end_monitor
+flag=1
+#file = "/usr/src/myFile"
+#/bin/cat <<EOM >$file
+#read status 5
+
+end_monitor()
 {
 	echo "Process Ends"
+	echo "Reaches end"
+	flag=0
+	echo "Flag $flag"
 	exit 1
 }
 
-function read_status
+read_status()
 {
+#trap "echo trap invoked" USR1
 #trap end_monitor USR1
+echo "Flag before if $flag"
+#if [ $flag==0 ]
+#then
+#	exit 1
+#fi
+echo "Completed if"
 temp=$(vcgencmd measure_temp)
-echo '{$1},(temp)' > file
+#cat '{$1},(temp)' > file.txt  <<EOF
+cat > myFile.txt <<EOF
+$1	$temp
+EOF
 sleep 5
-read_status {$1}+5
+val=$1+5
+read_status $val
+#trap end_monitor USR1
 }
